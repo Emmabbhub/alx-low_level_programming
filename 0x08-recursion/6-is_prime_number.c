abc178fd4b5c6b48367b87b5328c8fa843843abe
Emmanuel Banjo <bolubanjo2@gmail.com>
2022-09-28 14:25:07 -0700

    a function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,27 @@
 #include "main.h"
 /**
- * prime2 - Makes possible to evaluate from 1 to n
- * @a: same number as n
- * @b: number that iterates from 1 to n
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
-**/
-int prime2(int a, int b)
-{
-	int prime2(int a, int b)
-		return (1);
-	else if (a % b == 0)
-		return (0);
-	return (prime2(a, b + 1));
-}
-/**
- * is_prime_number - checks if a number is prime
- * @n: Number Integer
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * is_prime_number - returns a prime number
+ * @n: takes in an integer
+ * Return: 1 if n a prime number or 0 if not prime
 **/
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	if (n == 1 || n < 0)
 		return (0);
-	return (prime2(n, 2));
+	return (pnumber(2, n));
+
+}
+/**
+ * pnumber - help me to find the prime number
+ * @x: integer
+ * @n: integer
+ * Return: 1 if prime or 0 if not
+**/
+int pnumber(int x, int n)
+{
+	if (n == x)
+		return (1);
+	else if (n % x == 0)
+		return (0);
+	return (pnumber(x + 1, n));
 }
