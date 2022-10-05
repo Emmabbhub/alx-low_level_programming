0f4168cc06f7a5dd8e331acddcc6ee9b2d51ca8d
Emmanuel Banjo <bolubanjo2@gmail.com>
2022-10-05 15:50:17 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checkes upper case
+ * @c: print int
+ * Return: give 1 or 0
+**/
+
+int _isupper(int c)
+{
+	int _isupper(int c)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
